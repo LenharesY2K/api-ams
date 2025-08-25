76c8b25d62b8d8f32079db82d00a16b1be1c5907
LenharesY2K <lenharespedro@gmail.com>
2025-08-25 18:19:45 -0300

    fernhub

--- cep.php
+++ cep.php
@@ -1,15 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Consultar Cep</title>
 </head>
 <body>
+    CEP:
     <input name="cep" id="cep" type="text" />
-    <button onlick = ""> Consultar Cep </button>
+    <button onclick="ConsultarCEP()">Consultar Cep</button>
+    <br/>
+    Endereço: <input type="text" id="endereco" />
+    <br/>
+    Cidade: <input type="text" id="cidade" />
+    <br/>
+    Estado: <input type="text" id="estado" />
+    <br/>
     <script>
-        var cep = document.getElementByid ("cep")
-        </script>
+        
+        function ConsultarCEP()
+        {
+            var cep = document.getElementById("cep")
+            let url = "https://viacep.com.br/ws/"+cep.value+"/json/"
+            fetch(url)
+            .then( resp => resp.json())
+            .then(dados => {
+                alert(dados.logradouro)
+            })
+        }
+    </script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,23 +1,16 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atividade de JavaScript</title>
-    <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-    crossorigin="anonymous"
-  >
-
+    <title>Página Inicial</title>
 </head>
 <body>
-    <center><h1>Revisão de Variaveis</h1></center>
+    <h1>Revisão Javascript</h1>
     <script>
+        const nome = "Fernando"
+        var numero = 11
         let idade = 35
-        var numero = 11 
-        const name = "Fernando"
 
         let meninas = [
             "Luana",
@@ -28,51 +21,41 @@
             "Nathally",
             "Shaden"
         ]
-        
-    for(let i = 0; i < meninas.length; i++)
-    {
-        document.write(meninas[i]+"<br/>")
-    }
 
-    let marcelo = {
-        "nome" : "Marcelo",
-        "idade" : "17",
-        "altura" : "1.61",
-        "peso"   : "62",
-
+        for(let i = 0; i < meninas.length; i++)
+        {
+            document.write(meninas[i]+"<br/>")
         }
-    
-    let pedro = {
-        "nome" : "Pedro",
-        "idade" : "17",
-        "altura" : "1.76",
-        "peso" : "80",
-    }
 
-    let yago = {
-        "nome" : "Yago",
-        "idade" : "18",
-        "altura" : "1.52",
-        "peso" : "43",
-    }
+        let marcelo = {
+            "Nome" : "Marcelo",
+            "Idade" : "17",
+            "Peso" : "62",
+            "Altura" : "1.61"
+        }
 
-    let meninos = [marcelo, pedro, yago]
+        let pedro = {
+            "Nome":"Pedro",
+            "Idade":"31",
+            "Peso":"70",
+            "Altura":"1.76"
+        }
 
-    for(let i = 0; i < meninos.length; i++)
-    {
-        document.write(meninos[i].nome+"<br/>")
-    }
+        
+
+        let yago = {
+            "Nome":"Yago",
+            "Idade":"17",
+            "Peso":"65",
+            "Altura":"1.64"
+        }
 
 
+        let meninos = [marcelo, pedro, yago]
+        for(let i = 0; i < meninos.length; i++)
+        {
+            document.write(meninos[i].Nome+"<br/>")
+        }
     </script>
-    
-
-
-
-    <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
-
 </body>
 </html>
