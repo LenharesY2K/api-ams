9f4920773d1e2ff3433930e768b8f46c9925164b
LenharesY2K <lenharespedro@gmail.com>
2025-08-27 14:37:34 -0300

    next alterations

---
+++ Templates/header.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FernHub</title>
+    <link rel="icon" href="IMG/favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="Templates/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/5f670eb272.js" crossorigin="anonymous"></script>
+</head>
+    <body>
+        <link href="style.css" rel="stylesheet">
+        <nav class="navbar navbar-expand-lg navbar">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#" aria-label="Menu">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" class="icon2"
+                        focusable="false">
+                        <rect y="4" width="24" height="2" rx="1" />
+                        <rect y="11" width="24" height="2" rx="1" />
+                        <rect y="18" width="24" height="2" rx="1" />
+                    </svg>
+                </a>
+                <a href="indexfer.php" class="navbar-center" aria-label="Página Principal">
+                    <img src="fernandogracianp.png" alt="Logo central" class="image" />
+                </a>
+                <div class="navbar-right-icons">
+                    <a href="#" aria-label="Buscar">
+                        <i class="fas fa-search icon2"></i>
+                    </a>
+                    <a href="#" aria-label="Usuário">
+                        <i class="fas fa-user icon2"></i>
+                    </a>
+                </div>
+            </div>
+        </nav>
--- indexfer.php
+++ indexfer.php
@@ -1,121 +1,21 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php include("Templates/header.php"); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acessando materiais</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/5f670eb272.js" crossorigin="anonymous"></script>
-</head>
+<BR><BR>
+<div class="container">
+    <a href="indexfer.php" style="text-decoration: none; color: inherit;">
+    <div class="card" style="width: 40rem;">
+        <div class="video-time">
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path d="M8 5v14l11-7z"/>
+            </svg>
+            <span id="videoDuration">14:27</span>
+        </div>
+        <img src="IMG/white.jfif" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="title-video ">Professor Fernando Graciano</p>
+            <p class="card-text ">PROFESSOR FERNANDO - professor ensina a todos os alunos e alunas a como criar uma api em react</p>
+        </div>
+    </div>
+</a>
 
-<body>
-    <style>
-        body {
-            height: 100%;
-            margin: 0;
-            background: black;
-            overflow: auto;
-            background-repeat: no-repeat;
-            background-size: cover;
-            /* cobre toda a área */
-            background-position: center;
-            /* centraliza a imagem */
-            background-attachment: fixed;
-        }
-
-        .image {
-
-            transition: transform 0.3s ease;/
-        }
-
-        .image:hover {
-            transform: scale(1.1);
-        }
-
-        .icon2 {
-
-            transition: transform 0.3s ease;/
-        }
-
-        .icon2:hover {
-            transform: scale(1.1);
-        }
-
-        .align_center {
-            align-items: center;
-            justify-content: center;
-            text-align: center;
-        }
-
-        .navbar {
-            background-color: rgb(12, 12, 12);
-            height: 75px;
-        }
-
-        /* Define a cor laranja para o ícone */
-        .navbar-brand svg {
-            fill: orange;
-            width: 30px;
-            height: 30px;
-        }
-
-        .navbar-center {
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%);
-        }
-
-        /* Ajusta para telas pequenas para não sobrepor */
-        @media (max-width: 991px) {
-            .navbar-center {
-                position: static;
-                transform: none;
-                margin-top: 0.5rem;
-                margin-bottom: 0.5rem;
-            }
-        }
-
-        .navbar-center img {
-            height: 80px;
-        }
-
-        .navbar-right-icons a {
-            color: white;
-            font-size: 1.5rem;
-            margin-left: 1rem;
-            text-decoration: none;
-        }
-    </style>
-    </head>
-
-    <body>
-        <nav class="navbar navbar-expand-lg navbar">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#" aria-label="Menu">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" class="icon2"
-                        focusable="false">
-                        <rect y="4" width="24" height="2" rx="1" />
-                        <rect y="11" width="24" height="2" rx="1" />
-                        <rect y="18" width="24" height="2" rx="1" />
-                    </svg>
-                </a>
-                <a href="https://exemplo.com" class="navbar-center" aria-label="Página Principal">
-                    <img src="fernandogracianp.png" alt="Logo central" class="image" />
-                </a>
-                <div class="navbar-right-icons">
-                    <a href="#" aria-label="Buscar">
-                        <i class="fas fa-search icon2"></i>
-                    </a>
-                    <a href="#" aria-label="Usuário">
-                        <i class="fas fa-user icon2"></i>
-                    </a>
-                </div>
-            </div>
-        </nav>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
-</body>
-
-</html>
+<?php include("Templates/footer.php"); ?>
